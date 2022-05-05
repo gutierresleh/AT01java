ec65b6f0fae2b2a67964ac297ada70d743580ad7
gutierresleh <102757734+gutierresleh@users.noreply.github.com>
2022-05-05 15:42:38 -0300

    Add files via upload

---
+++ AT01.js
@@ -0,0 +1,39 @@
+//1
+
+function nome (l){
+    console.log (l)
+    return
+ }
+
+ nome('leticia')
+
+ //2
+
+ function soma (h,e){
+    console.log(h+e)
+    return
+}
+
+soma(8,3)
+
+//3
+
+function soma (f,j){
+    console.log(f*j)
+    return
+}
+
+soma(7,9)
+
+//4
+
+function conta (r,s,t,u){
+    let v = (r+s+t+u)/4;
+    console.log (v)
+    return
+}
+
+conta(7,9,8,10)
+
+
+
